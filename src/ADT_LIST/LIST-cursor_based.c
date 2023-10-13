3a80ee601bb4ba465231a75d40d3c2b763510357
Akwa <110759413+Akwa-08@user.noreply.github.com>
2023-10-14 05:52:43 +0800

    cursor based

--- src/ADT_LIST/LIST-cursor_based.c
+++ src/ADT_LIST/LIST-cursor_based.c
@@ -15,11 +15,17 @@ typedef int LIST;
 
 void initVH(VirtualHeap *VH);
 int allocSpace(VirtualHeap *VH);
-void deallocSpace();
+void deallocSpace(VirtualHeap *VH, int ndx);
+void insertElem(LIST *head, char data, int pos);
+void deleteElem(LIST *head, char data);
+int locateElem(LIST head, char data);
+void displayList(LIST head);
+
+
+VirtualHeap VH;
 
 int main(){
     LIST head = -1;
-    VirtualHeap VH;
     char elem;
 
 }
@@ -44,9 +50,67 @@ int allocSpace(VirtualHeap *VH){
     return ndx;
 }
 
-void deallocSpace(VirtualHeap *VH, int index){
+void deallocSpace(VirtualHeap *VH, int ndx){
     if ( ndx > -1 && ndx < SIZE){
         VH->Nodes[ndx].link = VH->avail;
         VH->avail = ndx;
     }
+}
+
+void insertElem(LIST *head, char data, int pos){
+    LIST *trav = head;
+    int ndx = 0;
+
+    while(*trav != -1 && ndx < pos){
+        trav = &VH.Nodes[*trav].link;
+        ndx++
+    }
+
+    LIST node = allocSpace(&VH);
+
+    if (node != -1){
+        VH.Nodes[node].elem = data;
+        VH.Nodes[node].link = *trav;
+        *trav = node;
+    }
+}
+
+void deleteElem(LIST *head, char data){
+    LIST *trav = head;
+
+    while (*trav != -1 && VH.Nodes[*trav].elem != data){
+        trav = VH.Nodes[*trav].link;
+    }
+
+    if(*trav != -1){
+        LIST temp = *trav;
+        *trav = VH.Nodes[temp].link;
+        deallocSpace(&VH, temp);
+
+    }
+}
+
+int locateElem(LIST head, char data){
+    int pos = -1;
+    LIST trav = head;
+    int ndx;
+
+    for (ndx = 0; trav != -1 && pos == -1; ndx++, trav = VH.Nodes[trav].link){
+        if (VH.Nodes[trav].elem == data){
+            pos = ndx;
+        }
+
+    }
+    
+}
+
+void displayList(LIST head){
+    LIST trav = head;
+
+    while (trav != -1){
+        printf("\t%d [ %c | %d ]\n", trav, VH.Nodes[trav].elem, VH.Nodes[trav].link);
+        trav = VH.Nodes[trav].link;
+    }
+
+    printf("\n");
 }
